009b45f9788319cfb638d0f2817e814366a36472
Gav Shields <65550958+Nizzer01@users.noreply.github.com>
2024-07-03 13:21:53 +0100

    Creation of basic web server, handlers & forms

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # go-server
 
 Basic go web server project
+
+Creats a web server localhost:8080
+
+hosts 3 forms
+
+- index/root
+- /hello
+- /form.html
+
---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func formHandeler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST request sucessful \n")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name: %s \n", name)
+
+	fmt.Fprintf(w, "Address: %s \n", address)
+
+}
+
+func helloHandeler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not fount", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "method not supported", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf(w, "hello!")
+
+}
+
+func main() {
+	fileserver := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileserver)
+	http.HandleFunc("/form", formHandeler)
+	http.HandleFunc("/hello", helloHandeler)
+
+	fmt.Println("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+
+}
